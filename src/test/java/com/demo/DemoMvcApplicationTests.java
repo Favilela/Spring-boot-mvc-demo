176b3087d3c52a4cec8bbea2da69ecd0b7b63b1a
Fabio Vilela <favil@hotmail.com>
2020-04-16 20:19:44 -0300

    Commit inicial

---
+++ src/test/java/com/demo/DemoMvcApplicationTests.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
