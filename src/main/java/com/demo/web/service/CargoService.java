2e8c1d1727192e0a0bdd5a161ab905666fbd7780
Fabio Vilela <favil@hotmail.com>
2020-04-22 17:07:29 -0300

    add Classe CargoService e Impl

---
+++ src/main/java/com/demo/web/service/CargoService.java
@@ -0,0 +1,19 @@
+package com.demo.web.service;
+
+import java.util.List;
+
+import com.demo.web.domain.Cargo;
+
+public interface CargoService {
+	
+	public void salvar(Cargo cargo);
+	
+	public void editar(Cargo cargo);
+	
+	public void excluir(Long id);
+	
+	Cargo buscarPorId(Long id);
+	
+	List<Cargo> buscarTodos();
+
+}
