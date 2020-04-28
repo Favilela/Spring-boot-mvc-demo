3f2bba819f58f2e177bb66880bb591fc48e66db5
Fabio Vilela <favil@hotmail.com>
2020-04-28 19:31:23 -0300

    Cargo Cadastrar - Controller e View

--- src/main/java/com/demo/web/controller/CargoController.java
+++ src/main/java/com/demo/web/controller/CargoController.java
@@ -1,15 +1,32 @@
 package com.demo.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.demo.web.domain.Cargo;
+import com.demo.web.domain.Departamento;
+import com.demo.web.service.CargoService;
+import com.demo.web.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 	
+	@Autowired
+	CargoService cargoService;
+	
+	@Autowired
+	DepartamentoService departamentoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
@@ -17,5 +34,20 @@ public class CargoController {
 	public String listar() {
 		return "/cargo/lista";
 	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		
+		cargoService.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo cadastrado com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDepartamentos(){
+		
+		return departamentoService.buscarTodos();
+	}
+	
 
 }
