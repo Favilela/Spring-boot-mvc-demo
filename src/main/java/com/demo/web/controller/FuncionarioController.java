03c22d01201a130f157a4cc2aac07c12a082235e
Fabio Vilela <favil@hotmail.com>
2020-04-29 19:30:59 -0300

    Funcionario Cadastrar - Controller e View

--- src/main/java/com/demo/web/controller/FuncionarioController.java
+++ src/main/java/com/demo/web/controller/FuncionarioController.java
@@ -1,21 +1,58 @@
 package com.demo.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.demo.web.domain.Cargo;
+import com.demo.web.domain.Funcionario;
+import com.demo.web.domain.UF;
+import com.demo.web.service.CargoService;
+import com.demo.web.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
-	
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Autowired
+	private CargoService cargoService;
+
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
-	
+
 	@GetMapping("/listar")
 	public String listar() {
 		return "/funcionario/lista";
+	}
+
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário cadastrado com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+
+	public List<Cargo> getCargos() {
+		
+		return cargoService.buscarTodos();
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		
+		return UF.values();
 	}
 
 }
